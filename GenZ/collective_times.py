bd98c0e8f0d1596c66b51538d287a86ad282d336
abambhaniya3 <abambhaniya3@gatech.edu>
2024-09-26 15:14:15 -0400

    Addnig A2A collective time

--- GenZ/collective_times.py
+++ GenZ/collective_times.py
@@ -30,3 +30,30 @@ def get_message_pass_time(data, system):
                       
     msg_pass_time = ((4.2e-6 + (2-1)*system.interchip_link_latency) +  data / system.interchip_mem_bw)*1000
     return msg_pass_time
+
+
+def get_A2A_time(data, num_A2A_nodes, system):
+    """get_A2A_time
+
+    Args:
+        data (int): Total message size (Bytes) per node to be exchanged in all-to-all.
+        num_A2A_nodes (int): Number of nodes participating in the all-to-all operation.
+        system (System object): Object of class System
+
+    Returns:
+        time (float): Total time (msec) to complete the All-to-All operation
+    """
+    ## BWeff = 4B/N if Ring of size N
+    ## BWeff = 4B/T if 2D Torus of size TxT 
+    
+    # M = E/ep * D/tp * F * B * bb
+    
+    # A2A time = Start Latency + (N-1) * Tlink + (N-1) * M / BW
+    # Where N is the number of nodes, M is the message size per node pair, 
+    # Tlink is the inter-chip link latency, and BW is the inter-chip memory bandwidth
+
+    message_size_per_pair = data / num_A2A_nodes
+    A2A_time = (5e-6 + (num_A2A_nodes - 1) * system.interchip_link_latency + 
+                (num_A2A_nodes - 1) * message_size_per_pair / system.interchip_mem_bw) * 1000
+
+    return A2A_time
