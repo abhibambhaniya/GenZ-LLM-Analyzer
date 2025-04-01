cb2448332a1a83eec52cd6e3b7919d56eaff380c
abambhaniya3 <abambhaniya3@gatech.edu>
2025-04-01 16:41:28 -0700

    Replacing get_power with get_energy

--- GenZ/power.py
+++ GenZ/power.py
@@ -1,7 +1,7 @@
 from pandas import DataFrame
 from GenZ.analyse_model import simplify_df
 from GenZ import Unit
-def get_power(
+def get_energy(
             df: DataFrame, 
             power: float = 1000,    ## Power in Watts
             power_breakdown: dict = {
@@ -12,26 +12,28 @@ def get_power(
             },
 ) -> float:
     '''
-    Calculate the used power of based on the utilization of various components
+    Calculate the used energy of based on the utilization of various components
     df: DataFrame: Model DF with operator wise breakdown of LLM model
-    power: float|dict{float}: Power consumption of various components in the platform
+    energy: float|dict{float}: Energy consumption of various components in the platform
             Components: Static, Compute, Memory, Network
             
-    Return: float: Used power in kWh
+    Return: float: Used energy in kWh
     '''
     df = simplify_df(df)
 
-    used_power = power_breakdown['Static']*power
+    total_energy_used = 0
+    static_power = power_breakdown['Static']*power
     compute_power = power_breakdown['Compute']*power
     memory_power = power_breakdown['Memory']*power
     network_power = power_breakdown['Network']*power
     unit = Unit() 
     for i in range(len(df)):
-        used_power += df.loc[i,f'Latency ({unit.unit_time})'] * (
-                    (compute_power*df.loc[i,'Compute Utilization']) + 
+        total_energy_used += df.loc[i,f'Latency ({unit.unit_time})'] * (
+                    (static_power) +
+                    (compute_power*df.loc[i,'Compute Utilization']) +
                     (memory_power*df.loc[i,'Memory Utilization']) +
                     (network_power*df.loc[i,'Communication Utilization']))
         
-    used_power = used_power / (1000 * 3600 * 1000)  ## Convert power from watts * msec to kWh
+    total_energy_used = total_energy_used / (1000 * 3600 * 1000)  ## Convert Energy from watts * msec to kWh
 
-    return used_power
+    return total_energy_used
